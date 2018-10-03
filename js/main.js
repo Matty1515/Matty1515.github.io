182c7e420ebabff7d17b6ef0e73a542c2288f32b
Matty1515 <32711827+Matty1515@users.noreply.github.com>
2018-10-03 17:22:23 +0100

    Round 6 Fixtures & Appearance Objects
    Added the round 6 fixtures and created object constructor + new objects for league appearances

--- js/main.js
+++ js/main.js
@@ -17,17 +17,24 @@ const bristolStats = {
 	points: "5",
 };
 
-document.getElementById('bristolTablePosition').innerHTML = bristolStats.position;
-document.getElementById('bristolTableName').innerHTML = bristolStats.name;
-document.getElementById('bristolTablePlayed').innerHTML = bristolStats.played;
-document.getElementById('bristolTableWon').innerHTML = bristolStats.won;
-document.getElementById('bristolTableDrawn').innerHTML = bristolStats.drawn;
-document.getElementById('bristolTableLost').innerHTML = bristolStats.lost;
-document.getElementById('bristolTableDifference').innerHTML = bristolStats.difference;
-document.getElementById('bristolTablePoints').innerHTML = bristolStats.points;
+// Constructor function for rugby players by appearances
+function Appearances(rank, name, club, year, apps, active) {
+    this.rank = rank;
+    this.playerName = name;
+    this.club = club;
+    this.yearsActive = year;
+	this.apps = apps;
+	this.currentlyActive = active;
+}
+// Rugby players by appearances as new objects
+const richardWigglesworth = new Appearances("1st", "Richard Wigglesworth", "Sale, Saracens", "2002-", 266, true);
+const steveBorthwick = new Appearances("2nd", "Steve Borthwick", "Bath, Saracens", "1998-2014", 265, false);
+const georgeChuter = new Appearances("3rd", "George Chuter", "Saracens, Leicester", "1997-2014", 262, false);
+const philDawson = new Appearances("3rd", "Phil Dawson", "Newcastle, Northampton, Worcester", "2001-2017", 262, false);
+// Use this to complete - https://en.wikipedia.org/wiki/Premiership_Rugby#Player_records
 
 });
 
-// Add an animated position banner (#1 Saracens, #2 Exeter etc.) to the top of the page? Use getElementById to link
-// the table results to the animated banner.
+// Save all team data into javascript objects and use it to create a league table, an animated banner (#1 Saracens, #2 Exeter etc.) and
+// fill in other pieces of text (the current leader is ###).
 
