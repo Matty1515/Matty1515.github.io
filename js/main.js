915069036e61d21bf05f9e2482531da2436ddf2f
Matty1515 <32711827+Matty1515@users.noreply.github.com>
2018-10-10 16:12:48 +0100

    Round 7 Fixtures & Appearances Constructor

--- js/main.js
+++ js/main.js
@@ -6,31 +6,30 @@ $('.card').on('mouseenter', event => {
   $(event.currentTarget).removeClass('card-zoom')
 })
 
-const bristolStats = {
-	position: "1st",
-	name: "Bristol Rugby",
-	played: "2",
-	won: "1",
-	drawn: "0",
-	lost: "1",
-	difference: "23",
-	points: "5",
+const stats = {
+	bristolStats: {
+		rank: 1,
+		name: "Bristol Rugby",
+		points: "5",
+	},
 };
 
 // Constructor function for rugby players by appearances
-function Appearances(rank, name, club, year, apps, active) {
-    this.rank = rank;
-    this.playerName = name;
-    this.club = club;
-    this.yearsActive = year;
-	this.apps = apps;
-	this.currentlyActive = active;
+const Appearances = (rank, name, club, year, apps, active) => {
+	return {
+		rank,
+		name,
+		club,
+		year,
+		apps,
+		active
+	}
 }
 // Rugby players by appearances as new objects
-const richardWigglesworth = new Appearances("1st", "Richard Wigglesworth", "Sale, Saracens", "2002-", 266, true);
-const steveBorthwick = new Appearances("2nd", "Steve Borthwick", "Bath, Saracens", "1998-2014", 265, false);
-const georgeChuter = new Appearances("3rd", "George Chuter", "Saracens, Leicester", "1997-2014", 262, false);
-const philDawson = new Appearances("3rd", "Phil Dawson", "Newcastle, Northampton, Worcester", "2001-2017", 262, false);
+const richardWigglesworth = Appearances("1st", "Richard Wigglesworth", "Sale, Saracens", "2002-", 266, true);
+const steveBorthwick = Appearances("2nd", "Steve Borthwick", "Bath, Saracens", "1998-2014", 265, false);
+const georgeChuter = Appearances("3rd", "George Chuter", "Saracens, Leicester", "1997-2014", 262, false);
+const philDawson = Appearances("3rd", "Phil Dawson", "Newcastle, Northampton, Worcester", "2001-2017", 262, false);
 // Use this to complete - https://en.wikipedia.org/wiki/Premiership_Rugby#Player_records
 
 });
