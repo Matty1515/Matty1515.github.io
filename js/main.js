5b4b1c17ba0067996104733484ee47b6c340c871
Matty1515 <32711827+Matty1515@users.noreply.github.com>
2018-10-19 21:45:37 +0100

    RWC Update and Sitemap
    Added new sitemap and got rugby world cup page up to date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
 
 $('.card').on('mouseenter', event => {
-  $(event.currentTarget).addClass('card-zoom')
+	$(event.currentTarget).addClass('card-zoom')
 }).on('mouseleave', event => {
-  $(event.currentTarget).removeClass('card-zoom')
+	$(event.currentTarget).removeClass('card-zoom')
 })
 
 const stats = {
@@ -30,7 +30,14 @@ const richardWigglesworth = Appearances("1st", "Richard Wigglesworth", "Sale, Sa
 const steveBorthwick = Appearances("2nd", "Steve Borthwick", "Bath, Saracens", "1998-2014", 265, false);
 const georgeChuter = Appearances("3rd", "George Chuter", "Saracens, Leicester", "1997-2014", 262, false);
 const philDawson = Appearances("3rd", "Phil Dawson", "Newcastle, Northampton, Worcester", "2001-2017", 262, false);
+const charlieHodgson = Appearances("5th", "Charlie Hodgson", "Sale, Saracens", "2000-2016", 254, false);
+const tomMay = Appearances("6th", "Tom May", "Newcastle, Northampton, London Welsh", "1999-2015", 247, false);
+const hughVyvyan = Appearances("7th", "Hugh Vyvyan", "Newcastle, Saracens", "1998-2012", 245, false);
+const simonShaw = Appearances("8th", "Simon Shaw", "Bristol, Wasps", "1997-2011", 237, false);
+const andyGoode = Appearances("9th", "Andy Goode", "Leicester, Saracens, Worcester, Wasps, Newcastle", "1998-2016", 236, false);
+const stuartHooper = Appearances("10th", "Stuart Hooper", "Saracens, Leeds, Bath", "2000-2016", 232, false);
 // Use this to complete - https://en.wikipedia.org/wiki/Premiership_Rugby#Player_records
+// Use this to print - https://stackoverflow.com/questions/42563503/print-object-data-in-table-jquery
 
 });
 
